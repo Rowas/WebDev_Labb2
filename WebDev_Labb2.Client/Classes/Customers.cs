e144cb1d3ba6997aece42ea03d9e9612f4f6ef7c
Andreas Lind-Sahlin <Rowas@users.noreply.github.com>
2025-03-19 11:23:16 +0100

    -

---
+++ WebDev_Labb2.Client/Classes/Customers.cs
@@ -0,0 +1,21 @@
+﻿namespace WebDev_Labb2.Client.Classes
+{
+    public class Customers
+    {
+        public string username { get; set; }
+        public string userlevel { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string mobile_number { get; set; }
+        public DeliveryAddress delivery_adress { get; set; }
+    }
+
+    public class DeliveryAddress
+    {
+        public string street { get; set; }
+        public string post_code { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+    }
+}
---
+++ WebDev_Labb2.Client/Classes/Products.cs
@@ -0,0 +1,24 @@
+﻿namespace WebDev_Labb2.Client.Classes
+{
+    public class Products
+    {
+        public int sku { get; set; }
+        public double price { get; set; }
+        public bool in_stock { get; set; }
+        public string name { get; set; }
+        public string? appearance { get; set; }
+        public double atomic_mass { get; set; }
+        public string category { get; set; }
+        public double density { get; set; }
+        public double melt { get; set; }
+        public double boil { get; set; }
+        public double number { get; set; }
+        public string phase { get; set; }
+        public string source { get; set; }
+        public string? bohr_model_image { get; set; }
+        public string summary { get; set; }
+        public string symbol { get; set; }
+        public string? cpk_hex { get; set; }
+        public string block { get; set; }
+    }
+}
