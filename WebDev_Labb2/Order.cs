66406bdd879efbd68de0465190eee296042f0465
Andreas Lind-Sahlin <Rowas@users.noreply.github.com>
2025-03-26 16:32:29 +0100

    Update layout and add order management features
    - Changed application title in MainLayout.razor to "MME".
    - Enhanced product detail presentation in ShopItem.razor with new layout components.
    - Updated _Imports.razor to include necessary namespaces.
    - Introduced new Order and ItemList classes for order management in Order.cs.

---
+++ WebDev_Labb2/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace WebDev_Labb2
+{
+    public class Order
+    {
+        public string username { get; set; }
+        public string order_id { get; set; }
+        public DateOnly order_date { get; set; }
+        public DateOnly delivery_date { get; set; }
+        public string status { get; set; }
+        public List<ItemList> item_list { get; set; }
+    }
+
+    public class ItemList
+    {
+        public int sku { get; set; }
+        public int quantity { get; set; }
+    }
+}
