27f7c8d1c4229a948b99b83181ca10af040c2ca0
Andreas Lind-Sahlin <Rowas@users.noreply.github.com>
2025-04-02 13:27:46 +0200

    Implement login feature and enhance UI components
    This commit introduces a login feature and restructures the project to improve customer and product management systems. Key changes include:
    
    - Defined `Customers` and `DeliveryAddress` classes in `Customers.cs`.
    - Added JWT handling in `JwtToken.cs`.
    - Updated `Order.cs` to manage orders and items effectively.
    - Expanded product properties in `Products.cs`.
    - Enhanced user experience with UI updates in `MainLayout.razor` and `NavMenu.razor`.
    - Improved administrative functionality in `AdminPage.razor`.
    - Added customer search functionality in `Clients.razor`.
    - Enhanced error handling in `Error.razor`.
    - Updated `Home.razor` for a welcoming message.
    - Structured login and registration forms in `Login-Client.razor`.
    - Improved product display in `Shop.razor` and `ShopItem.razor`.
    - Updated `launchSettings.json` for new development URLs.
    - Added a custom icon with `favicon.ico`.
    - Updated `API-Spec.md` to include login API details.

--- Program.cs
+++ Program.cs
@@ -10,17 +10,12 @@ builder.Services.AddHttpClient();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -34,8 +29,6 @@ app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(WebDev_Labb2.Client._Imports).Assembly);
+    .AddInteractiveServerRenderMode();
 
 app.Run();
