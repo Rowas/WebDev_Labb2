1c73b4d7cfdebf8ae065f02ba1361c7d32e780e8
Andreas Lind-Sahlin <Rowas@users.noreply.github.com>
2025-04-08 07:29:31 +0200

    -

--- Model/Classes/Products.cs
+++ Model/Classes/Products.cs
@@ -7,7 +7,7 @@
         public bool in_stock { get; set; }
         public string name { get; set; }
         public string? appearance { get; set; }
-        public double atomic_mass { get; set; }
+        public double? atomic_mass { get; set; }
         public double? boil { get; set; }
         public string category { get; set; }
         public double? density { get; set; }
@@ -15,16 +15,16 @@
         public double? melt { get; set; }
         public double? molar_heat { get; set; }
         public string? named_by { get; set; }
-        public int number { get; set; }
-        public int period { get; set; }
-        public int group { get; set; }
-        public string phase { get; set; }
+        public int? number { get; set; }
+        public int? period { get; set; }
+        public int? group { get; set; }
+        public string? phase { get; set; }
         public string? source { get; set; }
         public string? bohr_model_image { get; set; }
         public string? bohr_model_3d { get; set; }
         public string? spectral_img { get; set; }
         public string summary { get; set; }
-        public string symbol { get; set; }
+        public string? symbol { get; set; }
         public int? xpos { get; set; }
         public int? ypos { get; set; }
         public int? wxpos { get; set; }
